6a049c5dabe86931687fa6d9fd0db286cd59bc10
gagginaspinnata <gagginaspinnata@gmail.com>
2023-10-31 11:18:33 +0000

    prova hook

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react"
 
 function App() {
+  const url = 'http://localhost:3000/incomes'
+  const [data, setData] = useState([]);
+
+  const fetchInfo = () => {
+    return fetch(url)
+      .then((res) => res.json())
+      .then((d) => setData(d))
+  }
+
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +33,9 @@ function App() {
           Learn React
         </a>
       </header>
+      {data.map((income, index)=>{
+        <p>{income.value}</p>
+      })}
     </div>
   );
 }
